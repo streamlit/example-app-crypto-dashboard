f208a95dba2e41d0c6cb4b140d31c11d9fb9a7e2
Charly Wargnier <cwar05@gmail.com>
2021-11-11 14:18:08 +0000

    Added st.set_page_config

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_icon="📈", page_title="Crypto Dashboard")
+
 st.sidebar.image(
     "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
     width=50,
